928e2c4985e08438e804a450e131cdede95cbf8b
unknown <pandaidancepat@gmail.com>
2022-11-25 11:54:58 +0700

    first commit

--- src/views/Tab3Page.vue
+++ src/views/Tab3Page.vue
@@ -2,28 +2,43 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 3</ion-title>
+        <ion-title>Button</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 3</ion-title>
+          <ion-title size="large">Button</ion-title>
         </ion-toolbar>
       </ion-header>
       
-      <ExploreContainer name="Tab 3 page" />
+      <ExploreContainer name="Warning Button" />
+      <ion-button color="danger" shape="round" @click="presentAlert">Warning Button</ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, alertController } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
 
 export default defineComponent({
   name: 'Tab3Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButton },
+  setup() {
+      const presentAlert = async () => {
+        const alert = await alertController.create({
+          header: 'Alert',
+          subHeader: 'Important message',
+          message: 'Error: Peringatan! [041643527][Damelia]<br>VM: {"fullscreen":true}<br>Info: native event handler',
+          buttons: ['OK'],
+        });
+
+        await alert.present();
+      };
+
+      return { presentAlert };
+    },
 });
 </script>
